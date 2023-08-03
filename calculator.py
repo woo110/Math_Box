bcd049d5373ac640056097a56710aa5441238d99
codeit <dnals7676@naver.com>
2023-08-03 16:41:19 +0900

    Add get_Remainder function

--- calculator.py
+++ calculator.py
@@ -1,5 +1,3 @@
-Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 #기본 계산기
 >>>def add(a, b):
 ...   return a+b
@@ -11,5 +9,9 @@ def divide_new(a,b):
 def multifle_new(a,b):
     reutrn a*b
 
+
 def getMedian(a, b):
     return (a+b)/2
+
+def getRemainder(a, b):
+    return a//b
